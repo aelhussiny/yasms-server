732ad1d118adf8a463dd1b676dc577c5c7fddf5f
aelhussiny <aelhussiny@esri.com>
2018-11-22 15:19:52 +0200

    Updated logic. See readme for new operations

--- app.js
+++ app.js
@@ -18,17 +18,31 @@ const db = new sqlite3.Database('./db/yasms-server.db', (err) => {
     }
 });
 
-const createTableSQL = "CREATE TABLE IF NOT EXISTS USERKEYS (username TEXT PRIMARY KEY, key TEXT NOT NULL)";
+const createUserKeysTableSQL = "CREATE TABLE IF NOT EXISTS USERKEYS (username TEXT PRIMARY KEY, key TEXT NOT NULL, address TEXT NOT NULL, lastupdated TEXT NOT NULL)";
+const createIdentitiesTableSQL = "CREATE TABLE IF NOT EXISTS IDENTITIES (identityname TEXT PRIMARY KEY, username TEXT NOT NULL, CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES USERKEYS(username) ON DELETE CASCADE)";
 
-db.run(createTableSQL, (err) => {
-    if (err) {
-        return console.error(err.message);
+db.run(createUserKeysTableSQL, (err1) => {
+    if (err1) {
+        return console.error(err1.message);
+    } else {
+        db.run(createIdentitiesTableSQL, (err2) => {
+            if (err2) {
+                return console.error(err2.message);
+            }
+            else {
+                db.run("PRAGMA foreign_keys = ON;", (err3) => {
+                    if (err3) {
+                        return console.error(err3.message);
+                    }
+                });
+            }
+        });
     }
 });
 
 app.post('/register', (req, res) => {
-    const registerstmt = db.prepare("INSERT INTO USERKEYS (username, key) VALUES (?, ?)");
-    registerstmt.run([req.body.username, req.body.key], (err) => {
+    const registerstmt = db.prepare("INSERT INTO USERKEYS (username, key, address, lastupdated) VALUES (?, ?, ?, DATETIME('now'))");
+    registerstmt.run([req.body.username, req.body.key, req.body.address], (err) => {
         if (err) {
             res.status(500);
             res.send(err);
@@ -40,26 +54,190 @@ app.post('/register', (req, res) => {
     });
 });
 
-app.all('/users', (req, res) => {
-    let searchstmt = "SELECT * FROM USERKEYS";
-    db.all(searchstmt, (err, rows) => {
+app.all('/requestchat/:identity', (req, res) => {
+    let searchstmt = "SELECT * FROM IDENTITIES WHERE identityname = (?)";
+    db.get(searchstmt, [req.params.identity], (err, row) => {
         if (err) {
             res.status(500);
             res.send(err);
         } else {
-            res.send(rows);
+            if (row) {
+                searchstmt = "SELECT key, address FROM USERKEYS WHERE username = (?)";
+                db.get(searchstmt, [row.username], (err2, row2) => {
+                    if (err2) {
+                        res.status(500);
+                        res.send(err);
+                    } else {
+                        if (row2) {
+                            res.send({
+                                "status": "success",
+                                "key": row2.key,
+                                "address": row2.address
+                            });
+                        } else {
+                            res.status(404);
+                            res.send({
+                                "error": "Identity not found",
+                                "code": 1
+                            });
+                        }
+                    }
+                });
+            } else {
+                res.status(404);
+                res.send({
+                    "error": "Identity not found",
+                    "code": 1
+                });
+            }
         }
     });
 });
 
-app.all('/users/:name', (req, res) => {
-    let searchstmt = "SELECT * FROM USERKEYS WHERE username = (?)";
-    db.all(searchstmt, [req.params.name], (err, rows) => {
+app.post('/addidentity', (req, res) => {
+    const searchstmt = "SELECT * FROM USERKEYS WHERE username = (?)";
+    db.get(searchstmt, [req.body.username], (err, row) => {
         if (err) {
             res.status(500);
             res.send(err);
         } else {
-            res.send(rows);
+            if (row) {
+                const username = row.username;
+                const publickey = new NodeRSA(row.key);
+                try {
+                    const decryptedUsername = publickey.decryptPublic(req.body.encryptedusername).toString('utf-8');
+                    if (decryptedUsername === username) {
+                        const insertstmt = db.prepare("INSERT INTO IDENTITIES (identityname, username) values (?, ?)");
+                        insertstmt.run([req.body.identityname, req.body.username], (err) => {
+                            if (err) {
+                                res.status(500);
+                                res.send(err);
+                            } else {
+                                res.send({
+                                    "status": "success"
+                                });
+                            }
+                        });
+                    } else {
+                        res.status(403);
+                        res.send({
+                            "error": "Incorrect key",
+                            "code": 3
+                        });
+                    }
+                } catch (error) {
+                    res.status(403);
+                    res.send({
+                        "error": "Incorrect key",
+                        "code": 2
+                    });
+                }
+            } else {
+                res.status(404);
+                res.send({
+                    "error": "Username not found",
+                    "code": 1
+                });
+            }
+        }
+    });
+});
+
+app.post('/deleteidentity', (req, res) => {
+    const searchstmt = "SELECT * FROM USERKEYS WHERE username = (?)";
+    db.get(searchstmt, [req.body.username], (err, row) => {
+        if (err) {
+            res.status(500);
+            res.send(err);
+        } else {
+            if (row) {
+                const username = row.username;
+                const publickey = new NodeRSA(row.key);
+                try {
+                    const decryptedUsername = publickey.decryptPublic(req.body.encryptedusername).toString('utf-8');
+                    if (decryptedUsername === username) {
+                        const deletestmt = db.prepare("DELETE FROM IDENTITIES WHERE identityname = (?)");
+                        deletestmt.run([req.body.identityname], (err) => {
+                            if (err) {
+                                res.status(500);
+                                res.send(err);
+                            } else {
+                                res.send({
+                                    "status": "success"
+                                });
+                            }
+                        });
+                    } else {
+                        res.status(403);
+                        res.send({
+                            "error": "Incorrect key",
+                            "code": 3
+                        });
+                    }
+                } catch (error) {
+                    res.status(403);
+                    res.send({
+                        "error": "Incorrect key",
+                        "code": 2
+                    });
+                }
+            } else {
+                res.status(404);
+                res.send({
+                    "error": "Username not found",
+                    "code": 1
+                });
+            }
+        }
+    });
+});
+
+app.post('/updateaddress', (req, res) => {
+    const searchstmt = "SELECT * FROM USERKEYS WHERE username = (?)";
+    console.log("GOT HERE!");
+    db.get(searchstmt, [req.body.username], (err, row) => {
+        if (err) {
+            res.status(500);
+            res.send(err);
+        } else {
+            if (row) {
+                const username = row.username;
+                const publickey = new NodeRSA(row.key);
+                try {
+                    const decryptedUsername = publickey.decryptPublic(req.body.encryptedusername).toString('utf-8');
+                    if (decryptedUsername === username) {
+                        const updatestmt = db.prepare("UPDATE USERKEYS SET address = (?), lastupdated = DATETIME('now') WHERE username = (?)");
+                        updatestmt.run([req.body.newaddress, req.body.username], (err) => {
+                            if (err) {
+                                res.status(500);
+                                res.send(err);
+                            } else {
+                                res.send({
+                                    "status": "success"
+                                });
+                            }
+                        });
+                    } else {
+                        res.status(403);
+                        res.send({
+                            "error": "Incorrect key",
+                            "code": 3
+                        });
+                    }
+                } catch (error) {
+                    res.status(403);
+                    res.send({
+                        "error": "Incorrect key",
+                        "code": 2
+                    });
+                }
+            } else {
+                res.status(404);
+                res.send({
+                    "error": "Username not found",
+                    "code": 1
+                });
+            }
         }
     });
 });
--- readme.md
+++ readme.md
@@ -8,29 +8,61 @@
 ### Starting the Server
 Inside the YASMS Server folder in your terminal of choice, run the command `node app.js`. This will start the server.
 The server will report to you what port it is running on. The default port is 3000.
+### Making Requests
+You'll need an app like [Postman](https://www.getpostman.com/) to make requests to the server.
 ### Available Operations
-YASMS Server currently supports 4 operations
+YASMS Server currently supports 6 operations:
 #### 1. /register
-The `/register` operation is a POST-only operation. It allows a new identity to be registered into the system.
+The `/register` operation is a POST-only operation. It allows a new user to be registered into the system.
 
 **_Parameters_**
 
-**username**: The username of the new identity
+**username**: The username of the new user.
 
-**key**: The PEM String of the RSA Public Key generated for the identity, and used by it to sign all messages
-#### 2. /users
-The `/users` operation supports both GET and POST methods. It lists all identities in the system and their public keys.
-#### 3. /users/`name`
-The `/users/name` operation supports both GET and POST methods. It allows seeing the username and public key for a specific user determined by the `name` parameter inside the URL path itself.
+**key**: The PEM String of the RSA Public Key generated for the identity, which verifies messages as signed by the corresponding private key held by the user.
 
-**_Parameters_**
-
-**name**: The username of the identity for which a public key is being requested
-#### 4. /unregister
-The `/unregister` operation is a POST-only operation. It allows the user to delete an identity, freeing the username for use by another user.
+***address**: The address the user is reachable at.
+#### 2. /updateaddress
+The `/updateaddress` operation is a POST-only operation. It allows the user to update the address at which he is reachable.
 
 **_Parameters_**
 
-**1. username**: The username of the identity to unregister
+**username**: The username of the user whose address is being updated.
 
-**2. encryptedusername**: The username of the identity to unregister encrypted with the user's private signing key
+**encryptedusername**: The username of the user encrypted with the user's private signing key. This is to ensure that the user himself, not an impersonator, wants to update the address.
+
+**newaddress**: The new address of the user.
+#### 3. /addidentity
+The `/addidentity` operation is a POST-only operation. It allows the user to add an identity to his existing list of identities.
+
+**_Parameters_**
+
+**username**: The username of the user for which an identity is to be added.
+
+**encryptedusername**: The username of the user encrypted with the user's private signing key. This is to ensure that the user himself, not an impersonator, wants to add the identity to his list.
+
+**identityname**: The name of the identity to be added.
+#### 4. /deleteidentity
+The `/deleteidentity` operation is a POST-only operation. It allows the user to remove an identity from his existing list of identities.
+
+**_Parameters_**
+
+**username**: The username of the user for which an identity is to be deleted.
+
+**encryptedusername**: The username of the user encrypted with the user's private signing key. This is to ensure that the user himself, not an impersonator, wants to delete the identity from his list.
+
+**identityname**: The name of the identity to be deleted.
+#### 5. /requestchat/`identity`
+The `/requestchat/identity` operation supports both GET and POST methods. It allows seeing the public key, and address for a specific identity determined by the `identity` parameter inside the URL path itself.
+
+**_Parameters_**
+
+**identity**: The name of the identity for which a public key and address are being requested.
+#### 6. /unregister
+The `/unregister` operation is a POST-only operation. It allows the user to delete himself and all his identities from the system.
+
+**_Parameters_**
+
+**username**: The username of the identity to unregister
+
+**encryptedusername**: The username of the identity to unregister encrypted with the user's private signing key. This is to ensure that the user himself, not an impersonator, wants to unregister from the system.
